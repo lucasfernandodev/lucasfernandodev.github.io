24ae755c7bca702c133921bae543c25797548208
Lucas Fernando <lucasfernando.dev@gmail.com>
2021-09-02 20:40:11 -0300

    feat(SEO): add pacote de seo

--- components/Utils/meta/index.tsx
+++ components/Utils/meta/index.tsx
@@ -1,27 +1,63 @@
 import NextHead from "next/head";
-import { useRouter } from "next/router";
-import og_image from "../../../public/SEO/og_home.png";
+import { NextSeo } from "next-seo";
 type HeadProps = {
   title: string;
 };
 
 const Head: React.FC<HeadProps> = ({ title }) => {
-  const router = useRouter();
-  console.log(router);
+  const description =
+    "Meu portfolio pessoal, contendo alguns dos meus projetos um puco de conhecimento sombre mim.";
+
   return (
-    <NextHead>
-      <title>{title}</title>
+    /* <title>
+        Lucas Fernando - Portfolio simples contendo um pouco da meu trabalho e
+        da minha historia.
+      </title>
+      <meta
+        name="title"
+        content="Lucas Fernando - Portfolio simples contendo um pouco da meu trabalho e da minha historia."
+      />
+
       <meta
         name="description"
         content="Meu portfolio pessoal, contendo alguns dos meus projetos um puco de conhecimento sombre mim."
-      ></meta>
-      <meta
+      /> */
+    <>
+      <NextSeo
+        title={title}
+        description={description}
+        openGraph={{
+          title: title,
+          description: description,
+          images: [
+            {
+              url: `${process.env.VERCEL_URL}/SEO/og_home.png`,
+              width: 800,
+              height: 600,
+              alt: "Lucas Fernando - Portfolio",
+            },
+          ],
+        }}
+      />
+      <NextHead>
+      <link rel="shortcut icon" href="SEO/favicon.svg" type="image/png" />
+        <meta
+          name="keywords"
+          content="HTML, CSS, JavaScript, Lucas Fernando, Portfolio"
+        ></meta>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="author" content="Lucas Fernando" />
+      </NextHead>
+    </>
+
+    /* <meta
         name="keywords"
         content="HTML, CSS, JavaScript, Lucas Fernando, Portfolio"
       ></meta>
+      /*
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="author" content="Lucas Fernando" />
-      <link rel="shortcut icon" href="SEO/favicon.svg" type="image/png" />
+  
 
       <meta property="og:title" content={title} />
       <meta property="og:type" content="website" />
@@ -29,7 +65,7 @@ const Head: React.FC<HeadProps> = ({ title }) => {
         property="og:description"
         content="Meu portfolio pessoal, contendo alguns dos meus projetos um puco de conhecimento sombre mim."
       />
-      <meta property="og:image" content={og_image.src} key="ogimage" />
+      <meta property="og:image" content="" key="ogimage" />
 
       <meta property="og:site_name" content={title} />
 
@@ -38,8 +74,7 @@ const Head: React.FC<HeadProps> = ({ title }) => {
       <meta
         name="twitter:description"
         content="Meu portfolio pessoal, contendo alguns dos meus projetos um puco de conhecimento sombre mim."
-      />
-    </NextHead>
+      /> */
   );
 };
 
