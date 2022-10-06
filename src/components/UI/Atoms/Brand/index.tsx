3b31f85f69e9aa432fc238ccdadccd5b6a2e4cf4
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-10-06 20:26:15 -0300

    chore: change website logo

--- src/components/UI/Atoms/Brand/index.tsx
+++ src/components/UI/Atoms/Brand/index.tsx
@@ -1,9 +1,14 @@
+import Link from '@/components/Utils/Link';
+import style from './style.module.css';
+
 const Brand = () => {
   return (
-    <div>
-      <img src="/imagens/logo.svg" alt="Lucas Fernando - Portfolio" />
-    </div>
+    <h1 className={style.brand}>
+      <Link href='/'>
+        <img src='/imagens/logo.svg' alt='Logo - Portifolio' />
+      </Link>
+    </h1>
   );
-}
+};
 
 export default Brand;
