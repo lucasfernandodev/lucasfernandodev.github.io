7e13eeddb3544b3a04356c2472178a16876e83ba
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-10-07 12:47:39 -0300

    style: removing animation

--- src/components/UI/Atoms/Navigation/index.tsx
+++ src/components/UI/Atoms/Navigation/index.tsx
@@ -1,6 +1,7 @@
 import style from "./style.module.css";
 import Link from "@/infra/Link";
 import Icon from "src/components/Utils/Icon";
+import { useEffect, useRef } from "react";
 
 type NavigationProps = {
   visibility?: boolean;
@@ -12,24 +13,24 @@ const Navigation: React.FC<NavigationProps> = ({visibility}) => {
     <nav className={style.navigation} data-show={visibility}>
       <ul>
         <li>
-          <Link href="/" aria-label="Home">
+          <Link href="/" aria-label="Home" data-display="false">
             <Icon icon="home"/>
           </Link>
         </li>
 
         <li>
-          <Link href="/me" aria-label="Sobre mim">
+          <Link href="/me" aria-label="Sobre mim" data-display="false">
             <Icon icon="me"/>
           </Link>
         </li>
 
         <li>
-          <Link href="/projects" aria-label="Projetos">
+          <Link href="/projects" aria-label="Projetos" data-display="false">
             <Icon icon="projects" />
           </Link>
         </li>
         <li>
-          <Link href="/contact" aria-label="Contato">
+          <Link href="/contact" aria-label="Contato" data-display="false">
             <Icon icon="contact" />
           </Link>
         </li>
