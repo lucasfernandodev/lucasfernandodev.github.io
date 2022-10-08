9dc675d67a2c062513023869ccb77e5a39832f9c
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-10-08 10:56:35 -0300

    style: fixing background problem

--- src/components/UI/Organisms/Main/index.tsx
+++ src/components/UI/Organisms/Main/index.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
-import styles from './style.module.css';
+import React, { useEffect, useState } from 'react';
+import style from './style.module.css';
 
 export default function Main({children}: {children: React.ReactNode}){
+  let timer: any = null;
+  const [loadingCount, seLoadingcount] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [classes, setClasses] = useState<string>([style.main, style.fadeIn].join(' '))
+
+  useEffect(() => {
+
+    timer = setInterval(() => {
+
+      if(loadingCount === 100){
+        clearInterval(timer)
+        setLoading(false)
+        setClasses(style.main)
+      }else{
+        const LoadingSize = loadingCount + 1
+        seLoadingcount(LoadingSize)
+      }
+    }, 35)
+
+    return () => {
+      clearInterval(timer);
+    }
+  }, [loadingCount])
+
   return (
-    <main className={styles.main}>
+    <main className={classes}>
+      {loading && 
+      <div className={style.loading}>
+        <div className={style.loadingThumb} style={{width: `${loadingCount}%`}}></div>
+      </div>
+      }
       {children}
     </main>
   )
