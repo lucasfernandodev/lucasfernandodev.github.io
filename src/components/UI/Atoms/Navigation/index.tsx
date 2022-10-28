747b8d58da0fdd4a2c07ed2cc1ed40caf926517e
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-10-28 16:27:13 -0300

    chore: add umbrella-icons-library

--- src/components/UI/Atoms/Navigation/index.tsx
+++ src/components/UI/Atoms/Navigation/index.tsx
@@ -1,36 +1,35 @@
 import style from "./style.module.css";
 import Link from "@/infra/Link";
-import Icon from "src/components/Utils/Icon";
+import { Home, User, Code, Send } from "umbrella-icons-library";
 
 type NavigationProps = {
   visibility?: boolean;
-}
+};
 
-const Navigation: React.FC<NavigationProps> = ({visibility}) => {
-
+const Navigation: React.FC<NavigationProps> = ({ visibility }) => {
   return (
     <nav className={style.navigation} data-show={visibility}>
       <ul>
         <li>
           <Link href="/" aria-label="Home" data-active="false">
-            <Icon icon="home"/>
+            <Home className={style.icon}/>
           </Link>
         </li>
 
         <li>
           <Link href="/me" aria-label="Sobre mim" data-active="false">
-            <Icon icon="me"/>
+            <User className={style.icon}/>
           </Link>
         </li>
 
         <li>
           <Link href="/projects" aria-label="Projetos" data-active="false">
-            <Icon icon="projects" />
+            <Code className={style.icon}/>
           </Link>
         </li>
         <li>
           <Link href="/contact" aria-label="Contato" data-active="false">
-            <Icon icon="contact" />
+            <Send className={style.icon}/>
           </Link>
         </li>
       </ul>
