8f0d7a6368deb215b8b7f7eee7df2e2d7edc2931
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-11-01 14:45:02 -0300

    style: removed buttons

--- src/components/Templates/Contact/index.tsx
+++ src/components/Templates/Contact/index.tsx
@@ -3,7 +3,7 @@ import Link from '@/infra/Link';
 import Icon from 'src/components/Utils/Icon';
 import Paragraph from 'src/components/UI/Atoms/Paragraph';
 import Title from '../../UI/Atoms/Title';
-import Button from '../../UI/Atoms/Button/root';
+import {Button} from '../../UI/Atoms/Button';
 import Layout from '@/infra/Layout';
 
 const ContactTemplate = () => {
@@ -26,44 +26,7 @@ const ContactTemplate = () => {
           </Link>
         </div>
         <div className={contact.projects}>
-            <ul>
-              <li>
-                <Button
-                  type='All'
-                  height='sl'
-                  to='https://github.com/lucasfernandodev'
-                >
-                  <Icon icon='github' /> | <span>Github</span>
-                </Button>
-              </li>
-              <li>
-                <Button
-                  type='All'
-                  height='sl'
-                  to='https://www.linkedin.com/in/frontlucasfernandodev/'
-                >
-                  <Icon icon='linkedin' /> | <span>LinkedIn</span>
-                </Button>
-              </li>
-              <li>
-                <Button
-                  type='All'
-                  height='sl'
-                  to='https://www.instagram.com/lucasfernandodev/'
-                >
-                  <Icon icon='instagram' /> | <span>Instagram</span>
-                </Button>
-              </li>
-              <li>
-                <Button
-                  type='All'
-                  height='sl'
-                  to='https://codepen.io/lucasfernandodev'
-                >
-                  <Icon icon='codepen' /> | <span>Code Pen</span>
-                </Button>
-              </li>
-            </ul>
+           {/* Icons sociais */}
           </div>
       </section>
     </Layout>
