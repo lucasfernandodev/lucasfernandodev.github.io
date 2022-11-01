d5b2a0e740b362400b2486b8be1603e8a6118b91
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-11-01 11:58:47 -0300

    refactor: improving loading component

--- src/components/UI/Atoms/Loading/index.tsx
+++ src/components/UI/Atoms/Loading/index.tsx
@@ -1,11 +1,11 @@
 import style from './style.module.css';
 
-export default function Loading({ completed }: { completed: number }) {
+export default function Loading({ duration }: { duration: number }) {
   return (
     <div className={style.loading}>
       <div
         className={style.indicator}
-        style={{ width: `${completed}%` }}
+        style={{ '--duration': `${duration}ms`} as any}
       ></div>
     </div>
   );
