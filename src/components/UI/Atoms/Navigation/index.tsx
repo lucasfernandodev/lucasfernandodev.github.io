71cb64c01260db57a33c974052a4317c2ceacd6c
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-11-07 21:55:24 -0300

    refactor: creating file with route informations

--- src/components/UI/Atoms/Navigation/index.tsx
+++ src/components/UI/Atoms/Navigation/index.tsx
@@ -1,6 +1,6 @@
 import style from "./style.module.css";
 import Link from "@/infra/Link";
-import { Home, User, Code, Send } from "umbrella-icons-library";
+import { Route, RouteProps } from "@/infra/routes/default";
 
 type NavigationProps = {
   visibility?: boolean;
@@ -10,28 +10,20 @@ const Navigation: React.FC<NavigationProps> = ({ visibility }) => {
   return (
     <nav className={style.navigation} data-show={visibility}>
       <ul>
-        <li>
-          <Link href="/" aria-label="Home" data-active="false">
-            <Home className={style.icon}/>
-          </Link>
-        </li>
-
-        <li>
-          <Link href="/me" aria-label="Sobre mim" data-active="false">
-            <User className={style.icon}/>
-          </Link>
-        </li>
-
-        <li>
-          <Link href="/projects" aria-label="Projetos" data-active="false">
-            <Code className={style.icon}/>
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact" aria-label="Contato" data-active="false">
-            <Send className={style.icon}/>
-          </Link>
-        </li>
+        {Object.values(Route).map((route: RouteProps) => {
+          const Icon = route.icon;
+          return (
+            <li>
+              <Link
+                href={route.path}
+                aria-label={route.displayName}
+                data-active="false"
+              >
+                <Icon className={style.icon} />
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
