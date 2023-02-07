f73ab098a30af9c0d8ec024d7157c5ed3c646cdd
Lucas Fernando <lucasfernando.dev@gmail.com>
2023-02-07 09:16:34 -0300

    chore: change text content

--- src/components/UI/Templates/Me/index.tsx
+++ src/components/UI/Templates/Me/index.tsx
@@ -12,21 +12,25 @@ const MeTemplate = () => {
         <div className={style.content} id="mainContent">
           <Title>Sobre mim</Title>
           <Paragraph>
-            Olá meu nome é Lucas Fernando, tenho 21 anos, moro em Goiás.
+            Meu nome é Lucas Fernando e sou desenvolvedor web front-end. Sou
+            iniciante nesse mundo de desenvolvimento e to sempre buscando
+            aprender e melhorar.
           </Paragraph>
 
           <Paragraph>
-            Sou desenvolvedor web front-end, apaixonado por tecnologia, sempre
-            explorando e testando novas tecnologias, buscando aprender e evoluir como programador.
+            Em meu tempo livre, gosto de participar de desafios de codificação
+            online ou desenvolver novos projetos pessoais (Geralmente
+            automatizando algo).
           </Paragraph>
-          <Paragraph>Tecnologias que estou usando:</Paragraph>
+
+          <Paragraph>Tecnologias que estou usando: </Paragraph>
 
           <div className={style.tech} tabIndex={0}>
-            <Techs tech="html5" title="HTML5"/>
-            <Techs tech="js" title="Javascript"/>
-            <Techs tech="css3" title="CSS3"/>
-            <Techs tech="react" title="React.js"/>
-            <Techs tech="nextJs" title="Next.js"/>
+            <Techs tech="html5" title="HTML5" />
+            <Techs tech="js" title="Javascript" />
+            <Techs tech="css3" title="CSS3" />
+            <Techs tech="react" title="React.js" />
+            <Techs tech="nextJs" title="Next.js" />
           </div>
         </div>
         <div className={style.image}>
@@ -35,7 +39,7 @@ const MeTemplate = () => {
               src="/imagens/Photo-Lucas-Fernando-Dev.png"
               width={340}
               height={350}
-              alt="Imagem mostrando o rosto de Lucas Fernando"
+              alt="Lucas Fernando"
             />
           </div>
         </div>
