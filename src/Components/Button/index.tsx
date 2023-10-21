e5ee95dc4991882fac5e7246ce5800e4573b7f50
Lucas Fernando <lucasfernando.dev@gmail.com>
2023-10-21 14:56:57 -0300

    ✨ feat: Add Button component

---
+++ src/Components/Button/index.tsx
@@ -0,0 +1,10 @@
+import style from './style.module.css';
+
+interface ButtonProps{
+  type?: 'primary' | 'secondary';
+  children: React.ReactNode
+}
+
+export const Button: React.FC<ButtonProps> = ({children, type}) => {
+  return <button data-type={type ?? 'primary'} className={style.btn}>{children}</button>
+}
