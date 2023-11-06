da0f77b22a9dd561fc349d596809f2e2c8cdf114
Lucas Fernando <lucasfernando.dev@gmail.com>
2023-11-06 15:42:11 -0300

    ✨ feat: Add 404  page

--- src/Pages/404/index.tsx
+++ src/Pages/404/index.tsx
@@ -1,18 +1,26 @@
-import { useRouteError } from 'react-router-dom';
 import style from './style.module.css';
 import { Layout } from '../../Components/Layout';
+import { Title } from '../../Components/Title';
+import { Paragraph } from '../../Components/Paragraph';
+import { Button } from '../../Components/Button';
+import { IconChevronLeft } from '@tabler/icons-react';
 
 
 const Error404 = () => {
-  const error = useRouteError() as any
 
   return (
-    <Layout>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <Layout className={style.layout}>
+      <div className={style.container}>
+      <Title>Oops! Página não encontrada.</Title>
+      <Paragraph>Infelismente não foi possivel encontrar a pagina que você está procurando.</Paragraph>
+      <Paragraph>Volte para a página inicial e continue explorando.</Paragraph>
+      <div className={style.containerButtons}>
+      <Button><IconChevronLeft />Voltar para a pagina inicial</Button>
+      </div>
+      </div>
+      <div className={style.container}>
+        <img src="/404.svg" alt="Astronauta segurando um doginho" />
+      </div>
     </Layout>
   )
 }
