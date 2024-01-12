bbd25b4804a0154840602f2694a43eb3c6fe676b
Lucas Fernando <lucasfernando.dev@gmail.com>
2024-01-12 13:56:34 -0300

    🔧 chore: Fixing error with router not working
    When directly accessing the website at a URL other than root, the page was not rendered and a 404 message from github was returned.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
 })
