854dedba8d95c03006e06ca7ee6045461165e24f
Lucas Fernando <lucasfernando.dev@gmail.com>
2024-06-27 04:57:53 -0300

    💄 style: Add animation for mobile menu
    * Fixing homepage button position

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,39 +1,54 @@
-import { IconCode, IconComponents, IconHome, IconUser } from '@tabler/icons-react';
-import style from './style.module.css';
-import { IconSend } from '@tabler/icons-react';
-import { Link, useLocation } from 'react-router-dom';
-import { useEffect, useRef, useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import {
+  IconCode,
+  IconComponents,
+  IconHome,
+  IconUser,
+} from "@tabler/icons-react";
+import style from "./style.module.css";
+import { IconSend } from "@tabler/icons-react";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { useTranslation } from "react-i18next";
 
 export const Navbar = () => {
-
-  const refMenu = useRef<HTMLUListElement>(null)
-  const refBtn = useRef<HTMLButtonElement>(null)
-  const [visibility, setVisibility] = useState(false)
-  const { pathname } = useLocation()
-  const { t } = useTranslation()
+  const refMenu = useRef<HTMLUListElement>(null);
+  const refBtn = useRef<HTMLButtonElement>(null);
+  const [visibility, setVisibility] = useState(false);
+  const { pathname } = useLocation();
+  const { t } = useTranslation();
 
   useEffect(() => {
     if (refMenu.current) {
       const item = refMenu.current.querySelector(`a[href="${pathname}"]`);
       if (item) {
-        item.setAttribute('data-active', "true")
+        item.setAttribute("data-active", "true");
       }
     }
+  }, []);
 
-  }, [])
+  function toggleVisibility(ev: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    const body = document.querySelector("body") as HTMLElement;
+    const button = ev.target as HTMLButtonElement;
 
-
-  useEffect(() => {
-    if(visibility){
-      (document.querySelector('body') as HTMLElement).style.overflow = 'hidden';
-    }else{
-      (document.querySelector('body') as HTMLElement).style.overflow = 'auto';
+    function showMenu(){
+      button.classList.remove(style.btnStartTransition);
+      setVisibility(!visibility);
+      body.style.overflow = "hidden";
     }
-  }, [visibility])
 
-  function toggleVisibility() {
-    setVisibility(!visibility)
+    function closeMenu(){
+      button.classList.remove(style.btnStartTransitionOut);
+      setVisibility(!visibility);
+         body.style.overflow = "auto";
+    }
+
+    if (!visibility === true) {
+      button.classList.add(style.btnStartTransition);
+      setTimeout(showMenu, 300);
+    } else {
+      button.classList.add(style.btnStartTransitionOut);
+      setTimeout(closeMenu, 300);
+    }
   }
 
   return (
@@ -43,26 +58,52 @@ export const Navbar = () => {
         ref={refBtn}
         className={style.toggleMenu}
         data-open={visibility}
-        >
+      >
         <span>toggle nenu</span>
       </button>
       <ul className={style.menu} ref={refMenu} data-visibility={visibility}>
         <li className={style.item}>
-          <Link to="/" title={t("homepage.navTitle")} className={style.Link}><IconHome /></Link>
+          <Link to="/" title={t("homepage.navTitle")} className={style.Link}>
+            <IconHome />
+          </Link>
         </li>
         <li className={style.item}>
-          <Link to="/sobremim" title={t("about_me.navTitle")} className={style.Link}><IconUser /></Link>
+          <Link
+            to="/sobremim"
+            title={t("about_me.navTitle")}
+            className={style.Link}
+          >
+            <IconUser />
+          </Link>
         </li>
         <li className={style.item}>
-          <Link to="/tecnologias" title={t("tech.navTitle")} className={style.Link}><IconCode /></Link>
+          <Link
+            to="/tecnologias"
+            title={t("tech.navTitle")}
+            className={style.Link}
+          >
+            <IconCode />
+          </Link>
         </li>
         <li className={style.item}>
-          <Link to="/projetos" title={t("projects.navTitle")} className={style.Link}><IconComponents /></Link>
+          <Link
+            to="/projetos"
+            title={t("projects.navTitle")}
+            className={style.Link}
+          >
+            <IconComponents />
+          </Link>
         </li>
         <li className={style.item}>
-          <Link to="/contato" title={t("contact.navTitle")} className={style.Link}><IconSend /></Link>
+          <Link
+            to="/contato"
+            title={t("contact.navTitle")}
+            className={style.Link}
+          >
+            <IconSend />
+          </Link>
         </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
